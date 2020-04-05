a8e73e31fca1958fda0fe34dbc9ab2bdf4405ffa
Andrea Salvatori - IU6FZL <andrea.salvatori92@gmail.com>
2020-04-05 22:32:09 +0200

    Update terraform file - more complete api

--- Terraform.tf
+++ Terraform.tf
@@ -18,11 +18,14 @@ terraform {
 }
 # ------------------------ --------------------- ----------------------
 
-# locals variables
+# ---------------------------------------------------------------
+# ------------------ LOCALS VARIABLES ---------------------------
 locals {
   availability_zone_names = ["eu-west-1"]
 }
 
+# ---------------------------------------------------------------
+# ------------------- INPUT VARIABLES ---------------------------
 # Input variable from the Command Line or as Environment variable(export TF_VAR_account_id=xxxxxxxxx)
 variable "account_id" {
   type        = string
@@ -31,15 +34,17 @@ variable "account_id" {
 
 # ---------------------------------------------------------------
 # ------------------- API GATEWAY -------------------------------
+resource "aws_api_gateway_rest_api" "tbot_RLC_api" {
+  name        = "TbotRetriveLastCommit"
+  description = "API for Telegram bot retrieve last commit of a project"
+}
+
+# ------------------------ API STAGES ---------------------------
+# ---- DEV ----
 resource "aws_api_gateway_stage" "dev" {
   stage_name    = "dev"
   rest_api_id   = aws_api_gateway_rest_api.tbot_RLC_api.id
   deployment_id = aws_api_gateway_deployment.dev.id
-}
-
-resource "aws_api_gateway_rest_api" "tbot_RLC_api" {
-  name        = "TbotRetriveLastCommit"
-  description = "API for Telegram bot retrieve last commit of a project"
 }
 
 resource "aws_api_gateway_deployment" "dev" {
@@ -48,44 +53,150 @@ resource "aws_api_gateway_deployment" "dev" {
   stage_name  = "dev"
 }
 
+# ------------------------ API RESOURCES ---------------------------
+# ---- /crittomane ----
 resource "aws_api_gateway_resource" "resource" {
-  path_part   = "repos"
+  path_part   = "crittomane"
   parent_id   = aws_api_gateway_rest_api.tbot_RLC_api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.tbot_RLC_api.id
 }
 
-resource "aws_api_gateway_method" "repos-post" {
+# ------------------------ API METHODS ---------------------------
+# ---- POST - /crittomane ----
+resource "aws_api_gateway_method" "default-post" {
   rest_api_id   = aws_api_gateway_rest_api.tbot_RLC_api.id
   resource_id   = aws_api_gateway_resource.resource.id
   http_method   = "POST"
   authorization = "NONE"
+  #request_models = { "application/json" = aws_api_gateway_model.tbot_RLC_default-model.name }
+  request_validator_id = aws_api_gateway_request_validator.tbot_RLC_all.id
 }
 
-resource "aws_api_gateway_method" "repos-get" {
-  rest_api_id   = aws_api_gateway_rest_api.tbot_RLC_api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method_settings" "s" {
+resource "aws_api_gateway_method_settings" "settings" {
   rest_api_id = aws_api_gateway_rest_api.tbot_RLC_api.id
   stage_name  = aws_api_gateway_stage.dev.stage_name
-  method_path = "${aws_api_gateway_resource.resource.path_part}/${aws_api_gateway_method.repos-post.http_method}"
+  method_path = "${aws_api_gateway_resource.resource.path_part}/${aws_api_gateway_method.default-post.http_method}"
 
   settings {
-    metrics_enabled = true
-    logging_level   = "INFO"
+    metrics_enabled         = true
+    logging_level           = "INFO"
+    throttling_rate_limit   = 1
+    throttling_burst_limit  = 2
   }
 }
 
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.tbot_RLC_api.id
   resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.repos-post.http_method
+  http_method             = aws_api_gateway_method.default-post.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.tbot_RLC_lambda.invoke_arn
+}
+
+# ------------------------ API RESPONSES ---------------------------
+# ---- 200 response ----
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.tbot_RLC_api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.default-post.http_method
+  status_code = "200"
+}
+# ---- 500 response ----
+resource "aws_api_gateway_method_response" "response_500" {
+  rest_api_id = aws_api_gateway_rest_api.tbot_RLC_api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.default-post.http_method
+  status_code = "502"
+}
+
+# ---------------------------------------------------------------
+# ------------------------ API MODELS ---------------------------
+resource "aws_api_gateway_request_validator" "tbot_RLC_all" {
+  name                        = "all"
+  rest_api_id                 = aws_api_gateway_rest_api.tbot_RLC_api.id
+  validate_request_body       = true
+  validate_request_parameters = true
+}
+
+resource "aws_api_gateway_model" "tbot_RLC_default-model" {
+  rest_api_id  = aws_api_gateway_rest_api.tbot_RLC_api.id
+  name         = "firstName"
+  description  = "a JSON schema for "
+  content_type = "application/json"
+
+  schema = <<EOF
+{
+  "type": "string"
+}
+EOF
+}
+
+# ---------------------------------------------------------------
+# ---------------------- CLOUDWATCH API -------------------------
+
+# ---------------------- api account -------------------------
+resource "aws_api_gateway_account" "tbot_RLC_api_account" {
+  cloudwatch_role_arn = aws_iam_role.tbot_RLC_api_cloudwatch_role.arn
+}
+
+# ---------------------------------------------------------------
+# ---------------- CLOUDWATCH API LOG GROUP ---------------------
+# This is to optionally manage the CloudWatch Log Group for the Lambda Function.
+# If skipping this resource configuration, also add "logs:CreateLogGroup" to the IAM policy below.
+resource "aws_cloudwatch_log_group" "tbot_RLC_api_group" {
+  name              = "/aws/api-gateway/${aws_api_gateway_rest_api.tbot_RLC_api.name}"
+  retention_in_days = 14
+}
+
+# ---------------------------------------------------------------
+# ------------------- API-GATEWAY IAM ROLE ----------------------
+resource "aws_iam_role" "tbot_RLC_api_cloudwatch_role" {
+  name = "tbot_RLC_api_cloudwatch_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "apigateway.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+# ---------------------------------------------------------------
+# ------------- CLOUDWATCH API IAM POLICY & ATTACH --------------
+resource "aws_iam_role_policy" "tbot_RLC_api_cloudwatch_logging" {
+  name = "tbot_RLC_api_cloudwatch_logging"
+  role = aws_iam_role.tbot_RLC_api_cloudwatch_role.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:PutLogEvents",
+                "logs:GetLogEvents",
+                "logs:FilterLogEvents"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
 }
 
 # ---------------------------------------------------------------
@@ -97,7 +208,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
   principal     = "apigateway.amazonaws.com"
 
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${local.availability_zone_names[0]}:${var.account_id}:${aws_api_gateway_rest_api.tbot_RLC_api.id}/*/${aws_api_gateway_method.repos-post.http_method}${aws_api_gateway_resource.resource.path}"
+  source_arn = "arn:aws:execute-api:${local.availability_zone_names[0]}:${var.account_id}:${aws_api_gateway_rest_api.tbot_RLC_api.id}/*/${aws_api_gateway_method.default-post.http_method}${aws_api_gateway_resource.resource.path}"
 }
 
 # ---------------------------------------------------------------
@@ -105,14 +216,14 @@ resource "aws_lambda_permission" "apigw_lambda" {
 resource "aws_lambda_function" "tbot_RLC_lambda" {
   filename      = "tbot-lambda/lambda.zip"
   function_name = "tbot_RLC_lambda"
-  role          = aws_iam_role.tbot_RLC_lambda_role.arn
+  role          = aws_iam_role.tbot_RLC_lambda_cloudwatch_role.arn
   #depends_on    = [aws_iam_role_policy_attachment.tbot_RLC_lambda_logs,aws_cloudwatch_log_group.tbot_RLC_lambda_group]
   depends_on    = [aws_iam_role_policy_attachment.tbot_RLC_lambda_logs]
   handler       = "tbot_RLC_lambda.main"
   runtime       = "provided"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  source_code_hash = filebase64sha256("lambda.zip")
+  source_code_hash = filebase64sha256("tbot-lambda/lambda.zip")
 
   environment {
     variables = {
@@ -122,9 +233,18 @@ resource "aws_lambda_function" "tbot_RLC_lambda" {
 }
 
 # ---------------------------------------------------------------
+# ------------------- CLOUDWATCH LAMBDA -------------------------
+# This is to optionally manage the CloudWatch Log Group for the Lambda Function.
+# If skipping this resource configuration, also add "logs:CreateLogGroup" to the IAM policy below.
+resource "aws_cloudwatch_log_group" "tbot_RLC_lambda_group" {
+  name              = "/aws/lambda/${aws_lambda_function.tbot_RLC_lambda.function_name}"
+  retention_in_days = 14
+}
+
+# ---------------------------------------------------------------
 # ------------------- LAMBDA IAM ROLE ---------------------------
-resource "aws_iam_role" "tbot_RLC_lambda_role" {
-  name = "tbot_RLC_lambda_role"
+resource "aws_iam_role" "tbot_RLC_lambda_cloudwatch_role" {
+  name = "tbot_RLC_lambda_cloudwatch_role"
 
   assume_role_policy = <<POLICY
 {
@@ -144,19 +264,10 @@ POLICY
 }
 
 # ---------------------------------------------------------------
-# ------------------- CLOUDWATCH LAMBDA -------------------------
-# This is to optionally manage the CloudWatch Log Group for the Lambda Function.
-# If skipping this resource configuration, also add "logs:CreateLogGroup" to the IAM policy below.
-resource "aws_cloudwatch_log_group" "tbot_RLC_lambda_group" {
-  name              = "/aws/lambda/${aws_lambda_function.tbot_RLC_lambda.function_name}"
-  retention_in_days = 14
-}
-
-# ---------------------------------------------------------------
 # ----------------- CLOUDWATCH LAMBDA IAM -----------------------
 # See also the following AWS managed policy: AWSLambdaBasicExecutionRole
-resource "aws_iam_policy" "tbot_RLC_lambda_logging" {
-  name        = "lambda_logging"
+resource "aws_iam_policy" "tbot_RLC_lambda_cloudwatch_logging" {
+  name        = "tbot_RLC_lambda_cloudwatch_logging"
   path        = "/"
   description = "IAM policy for logging from a lambda"
 
@@ -177,10 +288,10 @@ resource "aws_iam_policy" "tbot_RLC_lambda_logging" {
 }
 EOF
 }
-
+# Attach Policy to te Role
 resource "aws_iam_role_policy_attachment" "tbot_RLC_lambda_logs" {
-  role       = aws_iam_role.tbot_RLC_lambda_role.name
-  policy_arn = aws_iam_policy.tbot_RLC_lambda_logging.arn
+  role       = aws_iam_role.tbot_RLC_lambda_cloudwatch_role.name
+  policy_arn = aws_iam_policy.tbot_RLC_lambda_cloudwatch_logging.arn
 }
 
 
