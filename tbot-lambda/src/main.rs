877e03cbf76d8475cf0423eded7fabecc3dea8b2
Andrea Salvatori - IU6FZL <andrea.salvatori92@gmail.com>
2020-04-05 22:29:58 +0200

    Test another rust code for Aws Lambda

--- tbot-lambda/src/main.rs
+++ tbot-lambda/src/main.rs
@@ -1,36 +1,45 @@
-use std::error::Error;
+mod lambda_gateway;
 
+use std::error::Error;
 use lambda_runtime::{error::HandlerError, lambda, Context};
-use log::{self, error};
 use serde_derive::{Deserialize, Serialize};
+use serde_json::{Value};
+use log::{info, warn, error};
 use simple_logger;
 use simple_error::bail;
 
-#[derive(Deserialize)]
-struct CustomEvent {
-    #[serde(rename = "firstName")]
-    first_name: String,
+use lambda_gateway::{LambdaRequest, LambdaResponse, LambdaResponseBuilder};
+
+/// This is the JSON payload we expect to be passed to us by the client accessing our lambda.
+#[derive(Deserialize, Debug)]
+struct InputPayload {
+    name: String,
 }
 
-#[derive(Serialize)]
-struct CustomOutput {
+/// This is the JSON payload we will return back to the client if the request was successful.
+#[derive(Serialize, Debug)]
+struct OutputPayload {
     message: String,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     simple_logger::init_with_level(log::Level::Debug)?;
-    lambda!(my_handler);
+    lambda!(lambda_handler);
 
     Ok(())
 }
 
-fn my_handler(e: CustomEvent, c: Context) -> Result<CustomOutput, HandlerError> {
-    if e.first_name == "" {
-        error!("Empty first name in request {}", c.aws_request_id);
-        bail!("Get me a name!!!!")
-    }
+fn lambda_handler(e: LambdaRequest<InputPayload>, _c: Context, ) -> Result<LambdaResponse, HandlerError> {
+    let payload = e.body();
+    info!("{:#?}", payload);
+    let name = &payload.name.to_uppercase();
 
-    Ok(CustomOutput {
-        message: format!("Hello, {}!", e.first_name),
-    })
+    let response = LambdaResponseBuilder::new()
+        .with_status(200)
+        .with_json(OutputPayload {
+            message: format!("Hi, '{}'", name),
+        })
+        .build();
+
+    Ok(response)
 }
