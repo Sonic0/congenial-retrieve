5dc6407d8c4637ac47f6906c144efb01193bc53b
Andrea Salvatori - IU6FZL <andrea.salvatori92@gmail.com>
2020-05-09 19:38:53 +0200

    Update Terraform DynamoDb resource

--- terraform/dynamo-db.tf
+++ terraform/dynamo-db.tf
@@ -7,7 +7,7 @@ resource "aws_dynamodb_table" "tbot_RLC_dynamodb" {
   read_capacity  = 5
   write_capacity = 5
   hash_key       = "UserId"
-  range_key      = "LastRetrieveDate"
+  range_key      = "Repo"
 
   attribute {
     name = "UserId"
@@ -15,54 +15,18 @@ resource "aws_dynamodb_table" "tbot_RLC_dynamodb" {
   }
 
   attribute {
-    name = "Username"
+    name = "Repo"
     type = "S"
   }
 
-  attribute {
-    name = "GithubToken"
-    type = "S"
-  }
-
-  attribute {
-    name = "Repos"
-    type = "S"
-  }
-
-  attribute {
-    name = "LastRetrieveDate"
-    type = "S" // ISO 8601 strings -> https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.NamingRulesDataTypes.html#HowItWorks.DataTypes
-  }
-
-  global_secondary_index {
-    name               = "RLC_Index"
-    hash_key           = "UserId"
-    range_key          = "Username"
-    write_capacity     = 3
-    read_capacity      = 3
-    projection_type    = "ALL" // projects every attribute into the index
-  }
-
-  global_secondary_index {
-    name               = "RLC_Index2"
-    hash_key           = "Username"
-    range_key          = "Repos"
-    write_capacity     = 3
-    read_capacity      = 3
-    projection_type    = "KEYS_ONLY" // projects just the hash and range key into the index
-  }
-
-  global_secondary_index {
-    name               = "RLC_Index3"
-    hash_key           = "UserId"
-    range_key          = "GithubToken"
-    write_capacity     = 3
-    read_capacity      = 3
-    projection_type    = "KEYS_ONLY" // projects just the hash and range key into the index
+  timeouts {
+    create = "3m"
+    update = "1m"
+    delete = "1m"
   }
 
   tags = {
-    Name        = "dynamodb-table-1"
+    Name        = "telegram-bot-table"
     Environment = "dev"
   }
 }
